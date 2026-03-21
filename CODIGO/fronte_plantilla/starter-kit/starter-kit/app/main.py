c863edfd0f4ce167c3297186eb71db7fc94909b1
Carlos Sebastián Rivera Espín <csriverae@gmail.com>
2026-03-20 19:20:41 -0500

    feat: add user schemas

--- CODIGO/fronte_plantilla/starter-kit/starter-kit/app/main.py
+++ CODIGO/fronte_plantilla/starter-kit/starter-kit/app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from app.api.routes.user import router
 
 app = FastAPI()
+
+app.include_router(router)
 
 
 @app.get("/health")
 async def health():
     return {"status": "ok"}
+
